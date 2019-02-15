2cefdc7f175219eb32d94d05014cb45f6560cff0
Brijesh Khandelwal <brkhande@microsoft.com>
2019-02-15 21:42:58 +0530

    Adding few Util files.

---
+++ ProducerPlugin/ProducerPlugin/NotifyRebuildEvent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerPlugin
+{
+    public class NotifyRebuildEvent<TKey, TValue>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        public string stateName;
+        public IAsyncEnumerable<KeyValuePair<TKey, TValue>> State { get; }
+
+        public NotifyRebuildEvent(string stateName, IAsyncEnumerable<KeyValuePair<TKey, TValue>> state)
+        {
+            this.stateName = stateName;
+            this.State = state;
+        }
+    }
+}
