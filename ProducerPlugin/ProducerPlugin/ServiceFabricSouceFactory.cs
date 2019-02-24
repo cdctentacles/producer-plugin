10cdfdf7294b4684ae96f7280420f0e868eba08a
Brijesh Khandelwal <brkhande@microsoft.com>
2019-02-24 14:34:20 +0530

    SourceFactory changed to ServiceFabricSourceFactory.cs

---
+++ ProducerPlugin/ProducerPlugin/ServiceFabricSouceFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Preview;
+using Microsoft.ServiceFabric.Data;
+using CDC.EventCollector;
+
+namespace ProducerPlugin
+{
+    internal class ServiceFabricSourceFactory
+    {
+        internal ReliableStateManager stateManager;
+        // Need to check the correct format here.
+        public ServiceFabricSourceFactory(ReliableStateManager stateManager)
+        {
+            this.stateManager = stateManager;
+        }
+        public Source CreateSource(IEventCollector collector, IHealthStore healthStore, string sourceName, Guid  partitionId)
+        {
+            return new ServiceFabricSource(collector, healthStore, sourceName, stateManager, partitionId);
+        }
+    }
+}
